3ff05c16bd679e87a0e4cdd74caf3bddab09e866
xiedejun <dejun@iscas.ac.cn>
2023-08-04 22:02:58 +0800

    feat: 提交配置文件

---
+++ bundle.js
@@ -0,0 +1,27 @@
+function parseUrl(url) {
+    const parsedUrl = new URL(url);
+    const params = new URLSearchParams(parsedUrl.search);
+    return params;
+}
+
+function appendVconsole(url) {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.async = true;
+    script.src =
+      "https://cdn.bootcdn.net/ajax/libs/vConsole/3.9.5/vconsole.min.js";
+    script.onload = function () {
+      // @ts-ignore
+      new VConsole();
+    };
+    document.body.appendChild(script);
+}
+
+function init() {
+    const params = parseUrl(window.location.href);
+    if (params.get("debug") == '1') {
+        appendVconsole();
+    }
+}
+
+module.exports = init;
